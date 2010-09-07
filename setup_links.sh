554ec15fad13e854aa79f184b51723e92f26c504
Adam Lesperance <lespea@gmail.com>
2010-09-06 23:16:36 -0500

    Add X/awesome configs

--- setup_links.sh
+++ setup_links.sh
@@ -1,6 +1,14 @@
 PWD=`pwd`
-ln -s ${PWD}/_vimrc 		~/.vimrc
-ln -s ${PWD}/vimfiles 		~/.vim
-ln -s ${PWD}/.emerald 		~
-ln -s ${PWD}/.emacs 		~
-ln -s ${PWD}/.bash_aliases 	~
+DEST=~
+CONFIG=${DEST}/.config
+mkdir -p ${CONFIG}
+
+ln -s ${PWD}/_vimrc 		${DEST}/.vimrc
+ln -s ${PWD}/vimfiles 		${DEST}/.vim
+ln -s ${PWD}/.emerald 		${DEST}
+ln -s ${PWD}/.emacs 		${DEST}
+ln -s ${PWD}/.bash_aliases 	${DEST}
+
+ln -s ${PWD}/X/.fonts.conf	${DEST}
+ln -s ${PWD}/X/.xinitrc	${DEST}
+ln -s ${PWD}/X/awesome	${CONFIG}/awesome

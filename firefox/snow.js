379ff1fa0d231a295c02742f44a183dd8568d593
Adam Lesperance <lespea@gmail.com>
2011-02-23 08:50:38 -0600

    My modifications to the reader snow theme

---
+++ firefox/snow.js
@@ -0,0 +1,2 @@
+    cssNode.innerHTML += '#reading-list-unread-count.unread-count, .unread-count.folder-unread-count.unread-count-d-1, .unread-count.sub-unread-count.unread-count-d-1, .name-text.folder-name-text.name-text-d-1 { color: #AA5555 !important; }';
+    cssNode.innerHTML += 'a.tree-link-selected .unread-count.folder-unread-count.unread-count-d-1, a.tree-link-selected .unread-count.sub-unread-count.unread-count-d-1, a.tree-link-selected .name-text.folder-name-text.name-text-d-1 { color: #FFF !important; }';

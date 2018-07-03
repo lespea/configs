7bfee9659faefab4bb92b7b5a05e62e96d68b983
Adam Lesperance <adam.lesperance@target.com>
2018-07-03 09:09:26 -0500

    Always start fresh with virts

--- setup_virts.sh
+++ setup_virts.sh
@@ -4,10 +4,16 @@ pip3 install virtualenvwrapper
 
 source `which virtualenvwrapper.sh`
 
+rm -rf ~/.virtualenvs/main
+rm -rf ~/.virtualenvs/main2
+rm -rf ~/.virtualenvs/nvim2
+rm -rf ~/.virtualenvs/nvim3
+
 mkvirtualenv nvim2 -p python2
 mkvirtualenv nvim3 -p python3
-mkvirtualenv main2 -p python2
+
 mkvirtualenv main -p python3
+mkvirtualenv main2 -p python2
 
 ~/.virtualenvs/main2/bin/pip install ipython request
 ~/.virtualenvs/main/bin/pip install ipython request

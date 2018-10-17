8ee4001325bc2f65a263a2ab7b71345d02cbb34a
Adam Lesperance <adam.lesperance@target.com>
2018-10-17 14:43:41 -0500

    Better setup files

--- setup_links.sh
+++ setup_links.sh
@@ -1,34 +1,96 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+CUR_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-PWD=`pwd`
-DEST=~
+OH_MY_ZSH_DIR="$HOME/.oh-my-zsh"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm -rf ${DEST}/.vimrc
-rm -rf ${DEST}/.vim
-rm -rf ${DEST}/.fonts
-rm -f ${DEST}/.emacs
-rm -f ${DEST}/.bash_aliases
+mkdir -p ${HOME}/.config
 
-ln -s ${PWD}/_vimrc         ${DEST}/.vimrc
-ln -s ${PWD}/vimfiles       ${DEST}/.vim
-ln -s ${PWD}/Fonts          ${DEST}/.fonts
-ln -s ${PWD}/.emacs         ${DEST}
-ln -s ${PWD}/.bash_aliases  ${DEST}
-
-if [ ! -f ${DEST}/.gitconfig ]; then
-    cp ${PWD}/myGitConfig ${DEST}/.gitconfig
+# Install oh-my-zsh
+install_omz=y
+if [[ -d "$OH_MY_ZSH_DIR" ]]; then
+    cd "$OH_MY_ZSH_DIR"
+    git pull
+    if [ $? -eq 0 ]; then
+        install_omz=n
+    else
+        rm -rf "$OH_MY_ZSH_DIR"
+        install_omz=y
+    fi
+    cd "$CUR_DIR"
+else
+    install_omz=y
 fi
 
-rm -f ${DEST}/.zshrc
-ln -s ${PWD}/.zshrc_mac  ${DEST}/.zshrc
+if [[ "$install_omz" == "y" ]]; then
+    echo "Installing oh my zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "Oh my zsh up to date"
+fi
 
-mkdir -p ${DEST}/.config
-ln -s ${PWD}/vimfiles ${DEST}/.config/nvim
+function setup_link() {
+    src="$1"
+    dst="$2"
 
-ln -s /usr/local/bin/python2 /usr/local/bin/python
+    if [[ "x$1" == "x" ]]; then
+        echo no src for setupLink func
+        exit 1
+    fi
 
-./setup_links.sh
+    if [[ "x$2" == "x" ]]; then
+        echo no dst for setupLink func
+        exit 1
+    fi
 
-\curl -L https://install.perlbrew.pl | bash
-perlbrew install perl-5.22.4 --thread --multi --64int --64all --ld --clang
+    if [[ -d "$dst" ]]; then
+        rm -rf "$dst"
+    elif [[ -f "$dst" ]]; then
+        rm -f "$dst"
+    fi
+
+    ln -s "$src" "$dst"
+}
+
+function setup_single() {
+    if [[ "x$1" == "x" ]]; then
+        echo no src for setupLink func
+        exit 1
+    fi
+
+    src="${CUR_DIR}/$1"
+    if [[ -f "$src" ]]; then
+        setup_link "$src" "${HOME}/$1"
+    else
+        echo weird src?
+        exit 1
+    fi
+}
+
+setup_link "${CUR_DIR}/_vimrc"        "${HOME}/.vimrc"
+setup_link "${CUR_DIR}/vimfiles"      "${HOME}/.vim"
+setup_link "${CUR_DIR}/Fonts"         "${HOME}/.fonts"
+
+setup_single .emacs
+setup_single .bash_aliases
+setup_single .gitattributes
+setup_single .ctags
+setup_single .ideavimrc
+
+if [ ! -f ${HOME}/.gitconfig ]; then
+    cp ${CUR_DIR}/myGitConfig ${HOME}/.gitconfig
+fi
+
+rm -f ${HOME}/.zshrc
+if [[ `uname` -eq "Darwin" ]]; then
+    setup_link "${CUR_DIR}/.zshrc_mac" "${HOME}/.zshrc"
+else
+    setup_single "${CUR_DIR}/.Xresources"
+
+    setup_link "${CUR_DIR}/.zshrc_arch" "${HOME}/.zshrc"
+    setup_link "${CUR_DIR}/.configs/i3" "${HOME}/.config/i3"
+fi
+
+setup_link "${CUR_DIR}/vimfiles" "${HOME}/.config/nvim"
+
+#\curl -L https://install.perlbrew.pl | bash
+#perlbrew install perl-5.22.4 --thread --multi --64int --64all --ld --clang
--- setup_virts.sh
+++ setup_virts.sh
@@ -1,22 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-pip3 install virtualenvwrapper
+pip3 install --upgrade virtualenvwrapper
 
 source `which virtualenvwrapper.sh`
 
-rm -rf ~/.virtualenvs/main
-rm -rf ~/.virtualenvs/main2
-rm -rf ~/.virtualenvs/nvim2
-rm -rf ~/.virtualenvs/nvim3
-
-mkvirtualenv nvim2 -p python2
-mkvirtualenv nvim3 -p python3
+rm -rf ~/.virtualenvs/main  2>/dev/null
+rm -rf ~/.virtualenvs/main2 2>/dev/null
+rm -rf ~/.virtualenvs/nvim2 2>/dev/null
+rm -rf ~/.virtualenvs/nvim3 2>/dev/null
 
 mkvirtualenv main -p python3
+mkvirtualenv nvim3 -p python3
+
 mkvirtualenv main2 -p python2
+mkvirtualenv nvim2 -p python2
 
 ~/.virtualenvs/main2/bin/pip install ipython request
-~/.virtualenvs/main/bin/pip install ipython request
+~/.virtualenvs/main/bin/pip  install ipython request
 
 ~/.virtualenvs/nvim2/bin/pip install neovim yapf
 ~/.virtualenvs/nvim3/bin/pip install neovim yapf

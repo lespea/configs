9dbd4fc9c2f550d3ec914df4a63c54172788131c
Adam Lesperance <lespea@gmail.com>
2020-03-10 03:27:53 -0500

    Better update script

--- update.sh
+++ update.sh
@@ -34,26 +34,12 @@ if [ $? -eq 0 ]; then
         echo 'Installing go'
         brew install go
     fi
-
-    which node >/dev/null 2>/dev/null
-    if [ $? -ne 0 ]; then
-        echo 'Installing node'
-        brew install node
-    fi
 fi
 
 which yay >/dev/null 2>/dev/null
 if [ $? -eq 0 ]; then
     yay
 fi
-
-echo 'Updating oh my zsh'
-cd "$HOME/.oh-my-zsh"
-git pull
-
-ycm="$SCRIPT_PATH/vimfiles/bundle/YouCompleteMe"
-cd "$ycm"
-OLD_YCM=`git rev-parse HEAD`
 
 echo 'Updating configs'
 cd "$SCRIPT_PATH"
@@ -62,25 +48,9 @@ git submodule update --recursive --init
 
 ./cleanup.sh
 
-args="--all"
-
-which ninja >/dev/null 2>/dev/null
-if [ $? -eq 0 ]; then
-    args="$args --ninja"
-fi
-
-echo 'Updating vim plugins'
-cd "$ycm"
-
-NEW_YCM=`git rev-parse HEAD`
-
-if [ $UPD_RUST -ne 0 ] || [ "$NEW_YCM" != "$OLD_YCM" ] || [ "z$1z" = "z1z" ]; then 
-    python3 ./install.py ${=args}
-else
-    echo "Not rebuilding YCM because nothing changed"
-fi
-
 source "$SCRIPT_PATH/antigen/bin/antigen.zsh"
 antigen update
 antigen reset
 antigen cleanup
+
+nvim +PlugInstall +PlugUpdate +qall

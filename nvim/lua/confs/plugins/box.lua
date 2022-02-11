d4143da1d3750e74177ac7c171bb842a8b015012
Adam Lesperance <adam.lesperance@target.com>
2022-02-11 10:37:53 -0600

    Increase the box width

--- nvim/lua/confs/plugins/box.lua
+++ nvim/lua/confs/plugins/box.lua
@@ -1,1 +1,1 @@
-local b=require('vimp')b.bind('nv','<leader>bb',function()return require('comment-box').lbox()end)b.bind('nv','<leader>bc',function()return require('comment-box').cbox()end)return b.bind('n','<leader>bl',function()return require('comment-box').line()end)
+require('comment-box').setup({doc_width=120,box_width=80})local b=require('vimp')b.bind('nv','<leader>bb',function()return require('comment-box').lbox()end)b.bind('nv','<leader>bc',function()return require('comment-box').cbox()end)return b.bind('n','<leader>bl',function()return require('comment-box').line()end)

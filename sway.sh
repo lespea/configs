c1e13e880e80ba6cdcca8f6cdbb472ded0f6657d
Adam Lesperance <lespea@gmail.com>
2022-11-17 07:55:05 -0600

    Prep for vulkan renderer

--- sway.sh
+++ sway.sh
@@ -4,13 +4,14 @@ if [[ -f "$HOME/.zsh_custom" ]]; then
     source "$HOME/.zsh_custom"
 fi
 
+export GDK_BACKEND=wayland
+export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland
 export SDL_VIDEODRIVER=wayland
+# export WLR_RENDERER=vulkan
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 export XDG_SESSION_TYPE=wayland
-export GDK_BACKEND=wayland
-export MOZ_ENABLE_WAYLAND=1
 export _JAVA_AWT_WM_NONREPARENTING=1
 
 exec dbus-run-session -- /usr/bin/sway

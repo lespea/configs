109f4c25b4c30fabf7d4a8fdef4b5d06881b8529
Adam Lesperance <lespea@gmail.com>
2023-01-15 10:45:16 -0600

    Update confs

--- sway.sh
+++ sway.sh
@@ -4,6 +4,9 @@ if [[ -f "$HOME/.zsh_custom" ]]; then
     source "$HOME/.zsh_custom"
 fi
 
+export XCURSOR_THEME=whiteglass
+export XCURSOR_SIZE=28
+
 export GDK_BACKEND=wayland
 export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland
@@ -13,5 +16,7 @@ export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 export XDG_SESSION_TYPE=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
+
+export SDL_DYNAMIC_API=/usr/lib/libSDL2.so
 
 exec dbus-run-session -- /usr/bin/sway

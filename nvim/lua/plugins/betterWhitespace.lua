a6ca6fa769120290542251ef66303366f5aa1a9a
Adam Lesperance <lespea@gmail.com>
2023-03-06 10:50:50 -0600

    No confirm for whitespace strip

--- nvim/lua/plugins/betterWhitespace.lua
+++ nvim/lua/plugins/betterWhitespace.lua
@@ -4,6 +4,7 @@ return {
     local g = vim.g
     g.strip_whitespace_on_save = 1
     g.strip_whitelines_at_eof = 0
+    g.strip_whitespace_confirm = 0
     g.strip_max_file_size = 1000
     g.show_spaces_that_precede_tabs = 1
   end

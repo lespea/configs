d72b85939a91d0deeb4d813f1759fc839292ef45
Adam Lesperance <lespea@gmail.com>
2023-03-26 19:43:44 -0500

    Update plugins

--- sway.sh
+++ sway.sh
@@ -4,19 +4,4 @@ if [[ -f "$HOME/.zsh_custom" ]]; then
     source "$HOME/.zsh_custom"
 fi
 
-export XCURSOR_THEME=whiteglass
-export XCURSOR_SIZE=28
-
-export GDK_BACKEND=wayland
-export MOZ_ENABLE_WAYLAND=1
-export QT_QPA_PLATFORM=wayland
-export SDL_VIDEODRIVER=wayland
-# export WLR_RENDERER=vulkan
-export XDG_CURRENT_DESKTOP=sway
-export XDG_SESSION_DESKTOP=sway
-export XDG_SESSION_TYPE=wayland
-export _JAVA_AWT_WM_NONREPARENTING=1
-
-export SDL_DYNAMIC_API=/usr/lib/libSDL2.so
-
-exec dbus-run-session -- /usr/bin/sway
+exec dbus-run-session -- /usr/bin/Hyprland

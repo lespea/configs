e7ef9c511dfe71f48836d0a296189f3391009c44
Adam Lesperance <lespea@gmail.com>
2023-03-28 00:53:55 -0500

    Better vimtex

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -2,8 +2,19 @@ return {
   'lervag/vimtex',
   lazy = false,
   config = function()
-    vim.g.vimtex_compiler_method = "tectonic"
-    vim.g.vimtex_view_general_viewer = 'okular'
-    vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
+    -- vim.g.vimtex_compiler_method = "lualatex"
+    -- vim.g.vimtex_view_general_viewer = 'okular'
+    vim.g.vimtex_view_general_viewer = 'zathura'
+    -- vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
+    -- vim.g.vimtex_compiler_latexmk = {
+    --   build_dir = '.out',
+    --   options = {
+    --     '-shell-escape',
+    --     '-verbose',
+    --     '-file-line-error',
+    --     '-interaction=nonstopmode',
+    --     '-synctex=1'
+    --   }
+    -- }
   end
 }

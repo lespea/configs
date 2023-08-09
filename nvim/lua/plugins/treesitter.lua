90dcde3b7ea6391578f9636eff7b9a9f4c24562b
Adam Lesperance <lespea@gmail.com>
2023-08-09 05:23:10 -0500

    ts rainbow breaks things

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -66,11 +66,6 @@ return {
           },
         },
 
-        rainbow               = {
-          enable = true,
-          extended_mode = true,
-        },
-
         refactor              = {
           highlight_definitions = {
             enable = true,
@@ -144,16 +139,16 @@ return {
       }
     end,
   },
-  {
-    'HiPhish/nvim-ts-rainbow2',
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        rainbow = {
-          enable = true,
-          query = 'rainbow-parens',
-          strategy = require 'ts-rainbow.strategy.global',
-        }
-      }
-    end
-  }
+  -- {
+  --   'HiPhish/nvim-ts-rainbow2',
+  --   config = function()
+  --     require("nvim-treesitter.configs").setup {
+  --       rainbow = {
+  --         enable = true,
+  --         query = 'rainbow-parens',
+  --         strategy = require 'ts-rainbow.strategy.global',
+  --       }
+  --     }
+  --   end
+  -- }
 }

d1f009b63b4c1b339eeb6a9db785fbc00be9e011
Adam Lesperance <lespea@gmail.com>
2023-10-24 01:21:20 -0500

    Use gpg agent directly

--- .configs/fish/config.fish
+++ .configs/fish/config.fish
@@ -3,6 +3,7 @@ pyenv init - | source
 fish_add_path ~/.cargo/bin
 
 if status is-interactive
-    keychain -q --agents gpg,ssh --eval id_ed25519 | source
+    gpg-connect-agent updatestartuptty /bye &>/dev/null
+    set -gx SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
 end
 

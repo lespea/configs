7edfe40724527c01c0ec408a0673ac9be0491888
Adam Lesperance <lespea@gmail.com>
2023-10-24 01:41:20 -0500

    Always start gpg-agent if we don't have it set

--- .configs/fish/config.fish
+++ .configs/fish/config.fish
@@ -3,6 +3,9 @@ pyenv init - | source
 fish_add_path ~/.cargo/bin
 
 if status is-interactive
+end
+
+if not string length --quiet $SSH_AUTH_SOCK
     gpg-connect-agent updatestartuptty /bye &>/dev/null
     set -gx SSH_AUTH_SOCK "$(gpgconf --list-dirs agent-ssh-socket)"
 end

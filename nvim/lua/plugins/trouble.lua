b7a1e84302995932f5a43be453f0ceca8a8fc3de
Adam Lesperance <adam.lesperance@target.com>
2023-11-14 18:50:40 -0600

    Use trouble

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,12 +1,19 @@
 return {
   'folke/trouble.nvim',
   opts = {},
-  keys = {
-    { '<leader>xx', '<CMD>TroubleToggle' },
+  init = function()
+    local t = require('trouble')
+    local set = vim.keymap.set
 
-    { '<leader>xw', '<CMD>TroubleToggle workspace_diagnostics' },
-    { '<leader>xd', '<CMD>TroubleToggle document_diagnostics' },
-    { '<leader>xq', '<CMD>TroubleToggle quickfix' },
-    { '<leader>xl', '<CMD>TroubleToggle loclist' },
-  }
+    set("n", "<leader>xx", function() t.toggle() end)
+    set("n", "<leader>xw", function() t.toggle("workspace_diagnostics") end)
+    set("n", "<leader>xd", function() t.toggle("document_diagnostics") end)
+    set("n", "<leader>xq", function() t.toggle("quickfix") end)
+    set("n", "<leader>xl", function() t.toggle("loclist") end)
+
+    set("n", "gR", function() t.toggle("lsp_references") end)
+
+    set("n", "<C-S-j>", function() t.next({ skip_groups = true, jump = true }); end)
+    set("n", "<C-S-k>", function() t.previous({ skip_groups = true, jump = true }); end)
+  end
 }

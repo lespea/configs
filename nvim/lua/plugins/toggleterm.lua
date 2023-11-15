752085e1eac4f912fcdf8d78b3e6d6ab72e1e106
Adam Lesperance <lespea@gmail.com>
2023-11-14 20:48:01 -0600

    Don't need the hterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -9,12 +9,10 @@ return {
 
     local lazygit = term:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
     local termFloat = term:new({ cmd = 'fish', hidden = true, direction = 'float' })
-    local term = term:new({ cmd = 'fish', hidden = true, direction = 'horizontal', size = 15 })
 
     local set = vim.keymap.set
 
     set({ 'n', 't' }, '~', function() termFloat:toggle() end)
-    set({ 'n', 't' }, '<c-s-t>', function() term:toggle() end)
     set({ 'n', 't' }, '<C-g>', function() lazygit:toggle() end)
   end
 }

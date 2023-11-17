07b9a4e5851a4437d8c749d6375b7a8b84173497
Adam Lesperance <adam.lesperance@target.com>
2023-11-17 13:30:56 -0600

    Check proj patterns over lsp

--- nvim/lua/plugins/project.lua
+++ nvim/lua/plugins/project.lua
@@ -2,6 +2,12 @@ return {
   'ahmedkhalf/project.nvim',
   config = function()
     require("project_nvim").setup {
+      -- Methods of detecting the root directory. **"lsp"** uses the native neovim
+      -- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
+      -- order matters: if one is not detected, the other is used as fallback. You
+      -- can also delete or rearangne the detection methods.
+      detection_methods = { "pattern", "lsp" },
+
       patterns = {
         ".git",
         "Makefile",

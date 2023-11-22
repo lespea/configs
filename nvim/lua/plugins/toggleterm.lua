dff769dc67aa65a4e1ead1f1b25a0331eee0ca31
Adam Lesperance <adam.lesperance@target.com>
2023-11-22 12:20:12 -0600

    Persistent horiz toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,19 +1,43 @@
 return {
   'akinsho/toggleterm.nvim',
-  opts = {
-    shell = 'fish',
-    --autochdir = true,
-  },
   init = function()
+    require('toggleterm').setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+
+      shell = 'fish'
+    })
+
     local term = require('toggleterm.terminal').Terminal
 
     local lazygit = term:new({ cmd = 'lazygit', hidden = true, direction = 'float' })
     local termFloat = term:new({ hidden = true, direction = 'float' })
+    local termRight = term:new({ hidden = true, direction = 'vertical' })
+    local termDown = term:new({ hidden = true, direction = 'horizontal' })
 
+    local ui = require("toggleterm.ui")
     local set = vim.keymap.set
 
     set({ 'n', 't' }, '<C-.>', function() termFloat:toggle() end)
     set({ 'n', 't' }, '<C-g>', function() lazygit:toggle() end)
     set({ 'n', 't' }, '<Esc>', '<C-\\><C-n>')
+
+    -- Persistent right term
+    set({ 'n', 't' }, '<C-,>', function()
+      if termRight:is_open() then
+        if termRight:is_focused() then
+          ui:goto_previous()
+        else
+          termRight:focus()
+        end
+      else
+        termRight:toggle()
+      end
+    end)
   end
 }

963044ac05055a28200e9f6820b8de8082e11ef7
Adam Lesperance <adam.lesperance@target.com>
2023-11-22 15:45:04 -0600

    The move keys don't work on mac so move to C-S-Up/Down

--- nvim/lua/plugins/vimMove.lua
+++ nvim/lua/plugins/vimMove.lua
@@ -1,3 +1,11 @@
 return {
   'matze/vim-move',
+  init = function()
+    vim.g.move_map_keys = 0
+
+    local set = vim.keymap.set
+
+    set('v', '<C-S-Up>', '<Plug>MoveBlockUp')
+    set('v', '<C-S-Down>', '<Plug>MoveBlockDown')
+  end
 }

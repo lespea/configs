69e6e35eb15f3ad4da2658a53bf60921e75431f1
Adam Lesperance <lespea@users.noreply.github.com>
2023-11-22 23:19:10 -0600

    Use cmake not make for fzf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -27,6 +27,6 @@ return {
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
-    build = 'make',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
   },
 }

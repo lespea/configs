a866e188184a60ae2fdb7fe3d691d765a3d4c6cd
Adam Lesperance <lespea@users.noreply.github.com>
2023-11-23 00:19:17 -0600

    Undo whichkey highlight

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -9,11 +9,5 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    triggers_blacklist = {
-      -- list of mode / prefixes that should never be hooked by WhichKey
-      -- this is mostly relevant for keymaps that start with a native binding
-      i = { "j", "k", "," },
-      v = { "j", "k" },
-    },
   }
 }

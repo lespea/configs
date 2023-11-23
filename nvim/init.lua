90503b76948cd8e57a243677181e32eedee50fbb
Adam Lesperance <lespea@users.noreply.github.com>
2023-11-23 00:12:54 -0600

    Fix lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 require('confs/00_init')
-require('confs/01_lazy')
 require('confs/10_settings')
+require('confs/11_lazy')
 require('confs/20_abbs')
 require('confs/21_augroup')
 require('confs/22_shortcuts')

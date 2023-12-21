55a7cb31bc6726b05bdcbc5c10c66998048528ec
Adam Lesperance <lespea@gmail.com>
2023-12-21 11:36:27 -0600

    Stop updating containers

--- .configs/fish/functions/tup.fish
+++ .configs/fish/functions/tup.fish
@@ -1,5 +1,5 @@
 function tup
-    set -l runo topgrade --disable gem --disable pip3 --disable vim --disable cargo --skip-notify
+    set -l runo topgrade --disable gem --disable pip3 --disable vim --disable cargo --disable containers --skip-notify
     if type -q mold
         mold --run $runo
     else

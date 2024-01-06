21c7173aab30ee9fdf8e0c8556b9662206fe8f51
Adam Lesperance <lespea@gmail.com>
2024-01-06 13:06:16 -0600

    Make sure vim python has all the needed packages

--- .configs/fish/functions/setupv.fish
+++ .configs/fish/functions/setupv.fish
@@ -5,6 +5,6 @@ function setupv
     pyenv virtualenv --clear --force $pyver $pyname
     pyenv shell $pyname
     pipbase
-    pipu colorama neovim python-lsp-server[all]
+    pipu colorama neovim python-lsp-server[all] flake8 prettier pyright ruff ruff-lsp
     pyenv shell normal
 end

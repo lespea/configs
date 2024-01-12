9449df981b244714ffc45778283ae824e3bff683
Adam Lesperance <adam.lesperance@target.com>
2024-01-12 17:34:08 -0600

    Include pyyaml in normal

--- .configs/fish/functions/pipnorm.fish
+++ .configs/fish/functions/pipnorm.fish
@@ -1,3 +1,3 @@
 function pipnorm --wraps='pipbase && pipu colorama ipython pandas numpy httpie requests' --description 'alias pipnorm pipbase && pipu colorama ipython pandas numpy httpie requests'
-  pipbase && pipu colorama ipython pandas numpy httpie requests $argv
+  pipbase && pipu colorama ipython pandas numpy httpie requests pyyaml $argv
 end

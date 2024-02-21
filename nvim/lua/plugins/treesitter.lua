b328065655141e5d847966f0fedcf2dc3bb9bb27
Adam Lesperance <adam.lesperance@target.com>
2024-02-21 13:39:40 -0600

    Turn off scope highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -73,7 +73,7 @@ return {
             -- Set to false if you have an `updatetime` of ~100.
             clear_on_cursor_move = true,
           },
-          highlight_current_scope = { enable = true },
+          highlight_current_scope = { enable = false },
           smart_rename = {
             enable = true,
             keymaps = {

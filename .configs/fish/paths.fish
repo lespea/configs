4ee1ab67b1e3f3d0e6986cc429c13639fb1d80df
Adam Lesperance <lespea@gmail.com>
2024-03-25 11:38:17 -0500

    Add locals bin to path

--- .configs/fish/paths.fish
+++ .configs/fish/paths.fish
@@ -3,6 +3,7 @@
 if set -q IS_ARCH
     fish_add_path -Up \
                       "$HOME/.cargo/bin" \
+                      "$HOME/.local/bin" \
                       "$HOME/.local/share/coursier/bin" \
                       "/usr/lib/ccache/bin" \
                       "$HOME/go/bin" \

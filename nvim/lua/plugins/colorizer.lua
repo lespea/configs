808718e0d5bb965957e6da1783aaef73430ac299
Adam Lesperance <lespea@gmail.com>
2024-05-18 16:09:59 -0500

    Better colorizer

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,4 +1,9 @@
 return {
-  'norcalli/nvim-colorizer.lua',
-  opts = {},
+  "NvChad/nvim-colorizer.lua",
+  event = "BufReadPost",
+  opts = {
+    user_default_options = {
+      names = false,
+    },
+  },
 }

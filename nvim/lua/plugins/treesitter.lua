cbaf84e56cd4427aeb072ddf0c1f7f92336d4b15
Adam Lesperance <lespea@gmail.com>
2024-05-18 15:53:11 -0500

    Fix rainbows

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -143,16 +143,4 @@ return {
       }
     end,
   },
-  -- {
-  --   'HiPhish/nvim-ts-rainbow2',
-  --   config = function()
-  --     require("nvim-treesitter.configs").setup {
-  --       rainbow = {
-  --         enable = true,
-  --         query = 'rainbow-parens',
-  --         strategy = require 'ts-rainbow.strategy.global',
-  --       }
-  --     }
-  --   end
-  -- }
 }

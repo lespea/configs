b985ef9f76987a9dc4d8cd691ad50be95df0cd98
Adam Lesperance <adam.lesperance@target.com>
2024-05-20 20:09:36 -0500

    Better keybinds for neo tree

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -29,14 +29,10 @@ return {
     },
     config = function()
       -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-      vim.fn.sign_define("DiagnosticSignError",
-        { text = " ", texthl = "DiagnosticSignError" })
-      vim.fn.sign_define("DiagnosticSignWarn",
-        { text = " ", texthl = "DiagnosticSignWarn" })
-      vim.fn.sign_define("DiagnosticSignInfo",
-        { text = " ", texthl = "DiagnosticSignInfo" })
-      vim.fn.sign_define("DiagnosticSignHint",
-        { text = "󰌵", texthl = "DiagnosticSignHint" })
+      vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+      vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+      vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+      vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
 
       require("neo-tree").setup({
         close_if_last_window = false,
@@ -310,7 +306,10 @@ return {
         }
       })
 
-      vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
+      local ntc = require('neo-tree.command')
+      local toggle = function() ntc.execute({ toggle = true }) end
+      vim.keymap.set({ 'n' }, "\\", toggle)
+      vim.keymap.set({ 'n' }, ",tt", toggle)
     end,
   },
 }

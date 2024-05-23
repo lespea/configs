32a4e8913525880f1126fe8a7cbf4e01432b8197
Adam Lesperance <adam.lesperance@target.com>
2024-05-23 11:08:58 -0500

    Fix trouble bindings

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -6,12 +6,12 @@ return {
     local set = vim.keymap.set
 
     set("n", "<leader>xx", function() t.toggle() end)
-    set("n", "<leader>xw", function() t.toggle("workspace_diagnostics") end)
-    set("n", "<leader>xd", function() t.toggle("document_diagnostics") end)
-    set("n", "<leader>xq", function() t.toggle("quickfix") end)
-    set("n", "<leader>xl", function() t.toggle("loclist") end)
+    set("n", "<leader>xw", function() t.open("workspace_diagnostics") end)
+    set("n", "<leader>xd", function() t.open("document_diagnostics") end)
+    set("n", "<leader>xq", function() t.open("quickfix") end)
+    set("n", "<leader>xl", function() t.open("loclist") end)
 
-    set("n", "gR", function() t.toggle("lsp_references") end)
+    set("n", "gR", function() t.open("lsp_references") end)
 
     set("n", "<C-S-j>", function() t.next({ skip_groups = true, jump = true }); end)
     set("n", "<C-S-k>", function() t.previous({ skip_groups = true, jump = true }); end)

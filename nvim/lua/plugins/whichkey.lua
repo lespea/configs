50105ad7d57cc7771a70a2f8ed36f96616f530a9
Adam Lesperance <lespea@gmail.com>
2024-07-13 15:02:24 -0500

    Add mini icons

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,5 +1,8 @@
 return {
   "folke/which-key.nvim",
+  dependencies = {
+    { 'echasnovski/mini.icons', version = false },
+  },
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true

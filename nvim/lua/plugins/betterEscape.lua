5f5289cb26928c948228d436ea9041def68c57d8
Adam Lesperance <adam.lesperance@target.com>
2024-07-18 14:34:29 -0500

    Update better escape

--- nvim/lua/plugins/betterEscape.lua
+++ nvim/lua/plugins/betterEscape.lua
@@ -1,6 +1,33 @@
 return {
   'max397574/better-escape.nvim',
   opts = {
-    mapping = { 'kj' }
+    default_mappings = false,
+    mappings = {
+      i = {
+        k = {
+          j = "<Esc>",
+        },
+      },
+      c = {
+        k = {
+          j = "<Esc>",
+        },
+      },
+      t = {
+        k = {
+          j = "<Esc>",
+        },
+      },
+      v = {
+        k = {
+          j = "<Esc>",
+        },
+      },
+      s = {
+        k = {
+          j = "<Esc>",
+        },
+      },
+    }
   },
 }

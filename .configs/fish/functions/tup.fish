1d634ec9c6b16f81fc5de7d0f69100eded4884b8
Adam Lesperance <adam.lesperance@target.com>
2024-09-16 13:40:43 -0500

    Unset github token in tup function

--- .configs/fish/functions/tup.fish
+++ .configs/fish/functions/tup.fish
@@ -1,4 +1,5 @@
 function tup
+    set -e GITHUB_API_TOKEN
     set -l runo topgrade --disable gem --disable pip3 --disable vim --disable cargo --disable containers --skip-notify
     if type -q mold
         mold --run $runo

596a26d3e85660073592d1d167b3e2b5baac5817
Adam Lesperance <adam.lesperance@target.com>
2024-12-17 14:30:24 -0600

    Cleanup metals

--- nvim/lua/plugins/metals.lua
+++ nvim/lua/plugins/metals.lua
@@ -1,14 +1,17 @@
+Filetypes = { 'java', 'scala', 'sbt' }
+
 return {
   'scalameta/nvim-metals',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'VonHeikemen/lsp-zero.nvim',
   },
-  ft = { 'scala', 'sbt' },
+  ft = Filetypes,
   config = function()
     local metals = require('metals')
 
     local conf = metals.bare_config()
+
     conf.settings = {
       showImplicitArguments = true,
       showInferredType = true,
@@ -20,13 +23,14 @@ return {
 
     -- metals.initialize_or_attach(conf)
     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+
     vim.api.nvim_create_autocmd("FileType", {
       -- NOTE: You may or may not want java included here. You will need it if you
       -- want basic Java support but it may also conflict if you are using
       -- something like nvim-jdtls which also works on a java filetype autocmd.
-      pattern = { "scala", "sbt" },
+      pattern = Filetypes,
       callback = function()
-        require("metals").initialize_or_attach(conf)
+        metals.initialize_or_attach(conf)
       end,
       group = nvim_metals_group,
     })

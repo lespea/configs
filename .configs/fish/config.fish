7a216a3fc369005ed899e4988821405d43442073
Adam Lesperance <adam.lesperance@target.com>
2024-12-19 16:06:59 -0600

    Rework fish conf

--- .configs/fish/config.fish
+++ .configs/fish/config.fish
@@ -1,67 +1,4 @@
-function final
-    set -l custom "$HOME/.fish_custom"
-    if test -e "$custom"
-        source $custom
-    end
-
-    if set -q IS_ARCH; and type -q uwsm; and uwsm check may-start
-        exec uwsm start hyprland.desktop
-    else
-        gvenv
-    end
-end
-
-if status is-interactive
-    if set -q IS_MAC; and set -q brewpath
-        $brewpath shellenv fish | source
-    end
-
-    ## Abbreviations
-
-    # set -l foreground f9fbff
-    # set -l selection 3b3d4d
-    # set -l comment 636572
-    # set -l red fe4c4c
-    # set -l orange fe854c
-    # set -l yellow ffc965
-    # set -l green 65ff91
-    # set -l purple b58cd8
-    # set -l sky 7fe7ff
-    # set -l pink ff99e0
-    #
-    # # Syntax Highlighting Colors
-    # set -g fish_color_normal $foreground
-    # set -g fish_color_command $sky
-    # set -g fish_color_keyword $pink
-    # set -g fish_color_quote $yellow
-    # set -g fish_color_redirection $foreground
-    # set -g fish_color_end $orange
-    # set -g fish_color_error $red
-    # set -g fish_color_param $purple
-    # set -g fish_color_comment $comment
-    # set -g fish_color_selection --background=$selection
-    # set -g fish_color_search_match --background=$selection
-    # set -g fish_color_operator $green
-    # set -g fish_color_escape $pink
-    # set -g fish_color_autosuggestion $comment
-    #
-    # # Completion Pager Colors
-    # set -g fish_pager_color_progress $comment
-    # set -g fish_pager_color_prefix $sky
-    # set -g fish_pager_color_completion $foreground
-    # set -g fish_pager_color_description $comment
-    # set -g fish_pager_color_selected_background --background=$selection
-
-    abbr --add G  -p anywhere '| rg'
-    abbr --add GA -p anywhere '| rg -M0'
-    abbr --add L  -p anywhere '| bat'
-    abbr --add LC -p anywhere '| wc -l'
-    abbr --add LP -p anywhere '| bat -p'
-    abbr --add BL -p anywhere '| bat -pP -llog'
-    abbr --add BJ -p anywhere '| jq | bat -ljson'
-    abbr --add UC -p anywhere '| sort | uniq -c | sort -rh'
-    abbr --add WC -p anywhere '| wc -l'
-
+function setEnvs
     set -gx JAVA_OPTS '-server -XX:+UseG1GC -Xmx8G -XX:-TieredCompilation'
     set -gx SBT_OPTS '-server -XX:+UseG1GC -Xmx8G -Xss2M -XX:ReservedCodeCacheSize=512M'
 
@@ -86,22 +23,81 @@ if status is-interactive
     end
 
     set -gx TAPLO_CONFIG "$XDG_CONFIG_HOME/taplo/taplo.toml"
+end
 
-    atuin init fish --disable-up-arrow | source
-    zoxide init fish | source
-    just --completions fish | source
-    rg --generate complete-fish | source
+function setAbbs
+    # search
+    abbr --add G  -p anywhere '| rg'
+    abbr --add GA -p anywhere '| rg -M0'
 
-    if type -q direnv
-        direnv hook fish | source
-    end
+    # bat
+    abbr --add L  -p anywhere '| bat'
+    abbr --add LP -p anywhere '| bat -p'
+    abbr --add BL -p anywhere '| bat -pP -llog'
 
+    # fmt
+    abbr --add BJ -p anywhere '| jq | bat -ljson'
+    abbr --add BL -p anywhere '| bat -pP -llog'
+    abbr --add BH -p anywhere '| prettier --parser html | bat -lhtml'
+
+    # fmt cursor
+    abbr --add TL --set-cursor 'tail -F % | bat -pP -llog'
+
+    # counts
+    abbr --add UC -p anywhere '| sort | uniq -c | sort -rh'
+    abbr --add LC -p anywhere '| wc -l'
+    abbr --add WC -p anywhere '| wc -l'
+
+    # misc
+    abbr --add g. -p anywhere './...'
+end
+
+function runSources
     if type -q mise
         mise activate fish | source
         mise completion fish | source
     end
 
+    act atuin init fish --disable-up-arrow
+    act zoxide init fish
+    act just --completions fish
+    act rg --generate complete-fish
+
+    act direnv hook fish
+
     bind . 'expand-dot-to-parent-directory-path'
+end
+
+function act
+    set -l cmd $argv[1]
+
+    if type -q $cmd
+        $cmd $argv[2..-1] | source
+    end
+end
+
+function final
+    set -l custom "$HOME/.fish_custom"
+    if test -e $custom
+        source $custom
+    end
+
+    if set -q IS_ARCH; and type -q uwsm; and uwsm check may-start
+        exec uwsm start hyprland.desktop
+    else
+        gvenv
+    end
+end
+
+if status is-interactive
+    if set -q IS_MAC; and set -q brewpath
+        $brewpath shellenv fish | source
+    end
+
+    setEnvs
+    setAbbs
+    runSources
 
     final
 end
+

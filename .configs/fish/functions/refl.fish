e78f152424824821a051f9ca884ade48ea68b3f9
Adam Lesperance <lespea@gmail.com>
2025-09-05 01:41:40 -0500

    Switch to sudo-rs

--- .configs/fish/functions/refl.fish
+++ .configs/fish/functions/refl.fish
@@ -1,4 +1,3 @@
 function refl --wraps="sudo reflector -n 24 -c 'United States' -f 10 -p https --save /etc/pacman.d/mirrorlist --threads 10 -a 12" --description "alias refl sudo reflector -n 24 -c 'United States' -f 10 -p https --save /etc/pacman.d/mirrorlist --threads 10 -a 12"
-  sudo reflector -n 24 -c 'United States' -f 10 -p https --save /etc/pacman.d/mirrorlist --threads 10 -a 12 $argv
-        
+  sudo-rs reflector -n 24 -c 'United States' -f 10 -p https --save /etc/pacman.d/mirrorlist --threads 10 -a 12 $argv
 end

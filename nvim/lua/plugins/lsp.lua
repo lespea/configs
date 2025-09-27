db2295cabf1a8e7aa6b962278156d9a2ce966b61
Adam Lesperance <lespea@gmail.com>
2025-09-26 22:14:28 -0500

    Add colorful menu

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,10 +9,18 @@ return {
 		opts = {}, -- required, even if empty
 	},
 	{
+		"xzbdmw/colorful-menu.nvim",
+		config = function()
+			-- You don't need to set these options.
+			require("colorful-menu").setup({})
+		end,
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			-- Configs
 			"neovim/nvim-lspconfig",
+			"xzbdmw/colorful-menu.nvim",
 			-- Autocompletion
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-nvim-lsp",
@@ -190,6 +198,21 @@ return {
 						cmp.config.compare.recently_used,
 						cmp.config.compare.kind,
 					},
+				},
+				formatting = {
+					format = function(entry, vim_item)
+						local highlights_info = require("colorful-menu").cmp_highlights(entry)
+
+						-- highlight_info is nil means we are missing the ts parser, it's
+						-- better to fallback to use default `vim_item.abbr`. What this plugin
+						-- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
+						if highlights_info ~= nil then
+							vim_item.abbr_hl_group = highlights_info.highlights
+							vim_item.abbr = highlights_info.text
+						end
+
+						return vim_item
+					end,
 				},
 			})
 

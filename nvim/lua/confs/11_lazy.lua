f86b01448eedf293d3aff3975e4bbf29d63bb1e8
Adam Lesperance <adam.lesperance@target.com>
2025-09-29 12:15:22 -0500

    Don't auto update plugins

--- nvim/lua/confs/11_lazy.lua
+++ nvim/lua/confs/11_lazy.lua
@@ -36,5 +36,5 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	-- install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = false },
 })

cd9acf4f4c8591ad8ead35a22c5667854b22fa96
Adam Lesperance <lespea@gmail.com>
2025-10-11 15:07:16 -0500

    Fix snippets paths

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -7,7 +7,7 @@ return {
 		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
-			require("luasnip.loaders.from_lua").load({ paths = "../snippets" })
+			require("luasnip.loaders.from_lua").load({ paths = { "../snippets" } })
 		end,
 	},
 }

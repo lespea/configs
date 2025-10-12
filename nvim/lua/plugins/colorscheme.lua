b53d3c11c9aa5211c9d90264d5c370da280418b0
Adam Lesperance <lespea@gmail.com>
2025-10-12 17:08:50 -0500

    Remove more unused things

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -24,38 +24,6 @@ return {
 			-- vim.api.nvim_set_hl(0, "Ibl2", { fg = c.gray4 })
 		end,
 	},
-	{
-		"navarasu/onedark.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		enabled = false,
-		-- Set our style and load our colorscheme
-		config = function()
-			local dark = require("onedark")
-			dark.setup({
-				style = "darker",
-			})
-			dark.load()
-		end,
-	},
-	{
-		"pauchiner/pastelnight.nvim",
-		lazy = false,
-		priority = 1000,
-		enabled = false,
-		config = function()
-			local theme = require("pastelnight")
-			local c = require("pastelnight.colors").highContrast()
-
-			theme.setup({
-				style = "highContrast",
-				terminal_colors = false,
-			})
-
-			vim.api.nvim_command([[colorscheme pastelnight-highcontrast]])
-			vim.api.nvim_set_hl(0, "LspInlayHint", { fg = c.base100 })
-		end,
-	},
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{
 		"rachartier/tiny-devicons-auto-colors.nvim",
@@ -78,15 +46,6 @@ return {
 					c.cyan,
 				},
 			})
-		end,
-	},
-	{
-		"loganswartz/sunburn.nvim",
-		enabled = false,
-		dependencies = { "loganswartz/polychrome.nvim" },
-		-- you could do this, or use the standard vimscript `colorscheme sunburn`
-		config = function()
-			vim.cmd.colorscheme("sunburn")
 		end,
 	},
 }

1bc1c3dc2f3e87bde6e29cb43e1b04308e44e523
Adam Lesperance <adam.lesperance@target.com>
2025-10-20 08:39:42 -0500

    Fix fish aliases

--- .configs/fish/functions/cp.fish
+++ .configs/fish/functions/cp.fish
@@ -1,4 +1,3 @@
 function cp --wraps='coreutils cp -g --reflink=auto' --description 'alias cp coreutils cp -g --reflink=auto'
     coreutils cp -g --reflink=auto $argv
-
 end
--- .configs/fish/functions/mv.fish
+++ .configs/fish/functions/mv.fish
@@ -1,4 +1,3 @@
 function mv --wraps='coreutils mv -g' --description 'alias mv coreutils mv -g'
     coreutils mv -g $argv
-
 end

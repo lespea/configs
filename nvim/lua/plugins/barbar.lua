20cdeea30bb449c7166ae485ee7c2b2c0d48b4b6
Adam Lesperance <adam.lesperance@target.com>
2025-10-21 00:16:17 -0500

    Few more tree/barbar fixes

--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -11,17 +11,7 @@ return {
 			preset = "slanted",
 		},
 		sidebar_filetypes = {
-			-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
-			NvimTree = true,
-			-- Or, specify the text used for the offset:
-			undotree = {
-				text = "undotree",
-				align = "center", -- *optionally* specify an alignment (either 'left', 'center', or 'right')
-			},
-			-- Or, specify the event which the sidebar executes when leaving:
 			["neo-tree"] = { event = "BufWipeout" },
-			-- Or, specify all three
-			Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
 		},
 	},
 	lazy = false,
@@ -55,4 +45,3 @@ return {
 		{ "<leader>bd", "<Cmd>BufferOrderByDirectory<CR>", opts, desc = "Order buffer by dir" },
 	},
 }
-

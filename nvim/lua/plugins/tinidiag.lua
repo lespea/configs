f43f236f3642ad6c06eb510a4022837acba3b7d0
Adam Lesperance <lespea@gmail.com>
2025-10-27 12:52:58 -0500

    Better tiny diagnostic

--- nvim/lua/plugins/tinidiag.lua
+++ nvim/lua/plugins/tinidiag.lua
@@ -3,7 +3,29 @@ return {
 	event = "VeryLazy",
 	priority = 1000,
 	config = function()
-		require("tiny-inline-diagnostic").setup()
+		require("tiny-inline-diagnostic").setup({
+			preset = "powerline",
+			options = {
+				set_arrow_to_diag_color = true,
+				show_all_diags_on_cursorline = true,
+
+				show_sources = {
+					enabled = true,
+					if_many = true,
+				},
+				add_messages = {
+					-- display_count = true,
+				},
+				multilines = {
+					enabled = true,
+				},
+				break_line = {
+					enabled = false, -- Enable automatic line breaking
+					after = 30, -- Number of characters before inserting a line break
+				},
+			},
+		})
 		vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+		vim.diagnostic.open_float = require("tiny-inline-diagnostic.override").open_float
 	end,
 }

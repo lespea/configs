ea3a0606597c1e1a7783a2e6ea3cb23304d2689d
Adam Lesperance <adam.lesperance@target.com>
2025-11-11 13:25:05 -0600

    Lazy spinner

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -297,10 +297,70 @@ return {
 			color = { bg = colors.purple, fg = colors.bg, gui = "italic,bold" },
 		}
 
+		local codeSpinner = require("lualine.component"):extend()
+
+		codeSpinner.processing = false
+		codeSpinner.spinner_index = 1
+
+		local spinner_symbols = {
+			"⠋",
+			"⠙",
+			"⠹",
+			"⠸",
+			"⠼",
+			"⠴",
+			"⠦",
+			"⠧",
+			"⠇",
+			"⠏",
+		}
+		local spinner_symbols_len = 10
+
+		-- Initializer
+		function codeSpinner:init(options)
+			codeSpinner.super.init(self, options)
+
+			local group = vim.api.nvim_create_augroup("CodeCompanionHooks", {})
+
+			vim.api.nvim_create_autocmd({ "User" }, {
+				pattern = "CodeCompanionRequest*",
+				group = group,
+				callback = function(request)
+					if request.match == "CodeCompanionRequestStarted" then
+						self.processing = true
+					elseif request.match == "CodeCompanionRequestFinished" then
+						self.processing = false
+					end
+				end,
+			})
+		end
+
+		-- Function that runs every time statusline is updated
+		function codeSpinner:update_status()
+			if self.processing then
+				self.spinner_index = (self.spinner_index % spinner_symbols_len) + 1
+				return spinner_symbols[self.spinner_index]
+			else
+				return nil
+			end
+		end
+
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
 				theme = theme,
+				ignore_focus = {
+					"Outline",
+					"codecompanion",
+					"edgy",
+					"neo-tree",
+					"qf",
+					"terminal",
+					"TERMINAL",
+					"toggleterm",
+					"trouble",
+				},
+				globalstatus = true,
 			},
 			sections = {
 				lualine_a = { "hostname", modes },
@@ -313,7 +373,7 @@ return {
 					"filetype",
 				},
 				lualine_y = { macro },
-				lualine_z = { cursor_line, cursor_col, "selectioncount", lsp },
+				lualine_z = { cursor_line, cursor_col, "selectioncount", codeSpinner, lsp },
 				-- lualine_z = { cursor_line, cursor_col, "selectioncount", dia, lsp },
 			},
 			inactive_sections = {

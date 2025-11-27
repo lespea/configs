c991bee0987fc8fc7a5b1b89a449eaba9c0fe4c0
Adam Lesperance <lespea@gmail.com>
2025-11-26 18:06:40 -0600

    Oil enhancement

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,20 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	{
+		"stevearc/oil.nvim",
+		opts = {
+			columns = {
+				"icon",
+				"permissions",
+				"size",
+				"mtime",
+			},
+		},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"benomahony/oil-git.nvim",
+		dependencies = { "stevearc/oil.nvim" },
+		opts = {},
+	},
 }

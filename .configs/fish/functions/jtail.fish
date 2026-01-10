fbe9cb9f7935d4b0f929771de0164db4fd6ef18b
Adam Lesperance <lespea@gmail.com>
2026-01-10 00:41:33 -0600

    Filter out ghostty logs

--- .configs/fish/functions/jtail.fish
+++ .configs/fish/functions/jtail.fish
@@ -1,3 +1,3 @@
 function jtail --wraps="clear && journalctl -n0 -f | rg -M0 -iv --line-buffered 'rtkit-daemon|G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START' \$argv | bat -pP -llog" --description "alias jtail clear && journalctl -n0 -f | rg -M0 -iv --line-buffered 'rtkit-daemon|G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START' \$argv | bat -pP -llog"
-    clear && journalctl -n0 -f | rg -M0 -iv --line-buffered 'rtkit-daemon|G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START' $argv | bat -pP -llog $argv
+    clear && journalctl -n0 -f | rg -M0 -iv --line-buffered 'rtkit-daemon|G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START|ghostty' $argv | bat -pP -llog $argv
 end

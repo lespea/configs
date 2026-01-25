b55703a95fd369be5919b374b47ac4f6375d03c1
Adam Lesperance <lespea@gmail.com>
2026-01-25 00:07:42 -0600

    Lock nexttest

--- cpkgs.py
+++ cpkgs.py
@@ -351,7 +351,7 @@ def get_packages(limit: set[str]) -> list[PkgInfo]:
         PkgInfo("cargo-edit"),
         PkgInfo("cargo-geiger"),
         PkgInfo("cargo-generate"),
-        PkgInfo("cargo-nextest"),
+        PkgInfo("cargo-nextest", locked=True),
         PkgInfo("cargo-outdated"),
         PkgInfo("cargo-show-asm"),
         PkgInfo("cargo-update"),

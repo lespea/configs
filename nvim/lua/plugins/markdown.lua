c56f8c76d54534c3640c988fc764e87da4abec96
Adam Lesperance <adam.lesperance@target.com>
2026-03-24 12:14:22 -0500

    Better markdown setup/config

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -5,8 +5,15 @@ return {
 		lazy = false, -- Recommended
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
+			"saghen/blink.cmp",
 		},
 		-- For `nvim-treesitter` users.
 		priority = 49,
+		config = function()
+			require("markview").setup()
+			require("markview.extras.checkboxes").setup()
+			require("markview.extras.headings").setup()
+			require("markview.extras.editor").setup()
+		end,
 	},
 }

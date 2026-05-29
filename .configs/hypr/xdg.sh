07eacd7773fc85390b6ff3c63197d94a91131979
Adam Lesperance <lespea@gmail.com>
2026-05-28 21:45:02 -0500

    Get xdg working again

--- .configs/hypr/xdg.sh
+++ .configs/hypr/xdg.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-sleep 1
+sleep 5
 killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2

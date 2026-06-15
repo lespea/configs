696c68e1e6cda0726c4f1bb9940b324a33627af4
Adam Lesperance <lespea@gmail.com>
2026-06-15 00:49:53 -0500

    Lock fnox

--- cpkgs.py
+++ cpkgs.py
@@ -365,7 +365,7 @@ def get_packages(limit: set[str]) -> list[PkgInfo]:
         PkgInfo("du-dust"),
         PkgInfo("eza"),
         PkgInfo("fd-find"),
-        PkgInfo("fnox"),
+        PkgInfo("fnox", locked=True),
         PkgInfo("fse_dump"),
         PkgInfo("git-delta"),
         PkgInfo("gitoxide"),

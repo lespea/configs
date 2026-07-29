319867be2bfa6b2eaeb5f6edd86bb499c1780a71
Adam Lesperance <lespea@gmail.com>
2026-07-28 19:23:21 -0500

    Fix editor

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ return {
 				-- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "/usr/share/hypr/stubs", words = { "hl" } },
 			},
 		},
 	},

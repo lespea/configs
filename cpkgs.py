867aaea02dbc7c98667244a1bf4a1ca0ff79bbbd
Adam Lesperance <lespea@gmail.com>
2026-09-06 10:58:16 -0500

    add smartedit

--- cpkgs.py
+++ cpkgs.py
@@ -391,6 +391,7 @@ def get_packages(limit: set[str]) -> list[PkgInfo]:
         PkgInfo("ripgrep_all"),
         PkgInfo("sd"),
         PkgInfo("simple-http-server"),
+        PkgInfo("smartedit-cli"),
         PkgInfo("splog"),
         PkgInfo("stylua"),
         PkgInfo("tokei", features=["all"]),
